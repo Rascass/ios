14bc215447db552bd8ea9948c83a238ee70d80bd
Rascass <40122506+Rascass@users.noreply.github.com>
2019-04-21 20:31:05 +0300

    Done

---
+++ Task3/Tasks/RSSchool_T3/Task4/RSSchool_T4/RSSchool_T4/ViewController.h
@@ -0,0 +1,21 @@
+//
+//  ViewController.h
+//  RSSchool_T4
+//
+//  Created by Mike Veber on 18.04.2019.
+//  Copyright © 2019 iOSLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController : UIViewController <UITextFieldDelegate>{
+	
+}
+@property(nonatomic, strong) UITextField *input;
+@property(nonatomic, strong) UIView *flags;
+
+@end
+
+NS_ASSUME_NONNULL_END
